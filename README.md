b20f3b4d1790775cf82c7b0e3107965c307131d4
igiri <igiri@guidewire.com>
2018-01-19 00:25:35 -0800

    Adding support for binance

---
+++ README.md
@@ -0,0 +1,12 @@
+**************** Readme **************************
+
+Watch your balances across different cryptocurrency exchanges. Supported exchanges are: Bittrex, Binance and GDAX.
+
+More exchanges will be added per demand.
+
+Usage: python balance.py --config config.yaml
+
+
+binance:
+  APIKey: [apikey-here]
+  Secret: [secretkey-here]
---
+++ balance.py
@@ -0,0 +1,87 @@
+import argparse
+import yaml
+
+from time import sleep
+from sys import platform
+from os import system
+from datetime import datetime
+
+HEADER = '''\
+_________                        __                         __         .__
+\_   ___ \_______ ___.__._______/  |_  ______  _  _______ _/  |_  ____ |  |__
+/    \  \/\_  __ <   |  |\____ \   __\/  _ \ \/ \/ /\__  \\\\   __\/ ___\|  |  \\
+\     \____|  | \/\___  ||  |_> >  | (  <_> )     /  / __ \|  | \  \___|   Y  \\
+\______  /|__|   / ____||   __/|__|   \____/ \/\_/  (____  /__|  \___  >___|  /
+       \/        \/     |__|                             \/          \/     \/
+
+         Created by: Ijesh Giri
+'''
+
+def print_header():
+    print(HEADER)
+
+def process_args():
+    parser = argparse.ArgumentParser(description='Watch balance across multiple exchanges!')
+    parser.add_argument('--config', default= 'config.yaml',help ='locaion of the config yaml file')
+
+    args = parser.parse_args()
+    return args
+
+def clear():
+    """
+    Output: Clears the terminal
+    """
+    if platform in ("linux", "linux2", "darwin"):
+        system("clear")
+    elif platform == "win32":
+        system("cls")
+    else:
+        print("Unsupported command for the system!")
+
+from binance.client import Client
+def main():
+    args = process_args()
+    with open(args.config) as keys:
+        info_dict = yaml.load(keys)
+        api_key = info_dict['binance']['APIKey']
+        api_secret = info_dict['binance']['Secret']
+
+        client = Client(api_key, api_secret)
+        watch_loop(client, 5)
+
+def watch_loop(client, sleep_time = 30):
+    try:
+        while True:
+            print("Fetched at: "+ str(datetime.now()))
+            account_info = client.get_account()
+            nonzero_balances = get_nonzero_balances(account_info['balances'])
+            sleep(sleep_time) #every 30 secs
+            print("\n*****************************************************\n")
+
+    except KeyboardInterrupt:
+        clear()
+        print_header()
+
+def get_nonzero_balances(balances):
+    nonzero_balances = {}
+    for balance in balances:
+        if(float(balance['free']) > 0.0):
+            print(balance['free'] + " " + balance['asset'])
+            nonzero_balances[balance['asset']] = float(balance['free'])
+
+        if(float(balance['locked']) > 0.0):
+            try:
+                value = nonzero_balances[balance['asset']] + float(balance['locked'])
+                print(str(value) + " " + balance['asset'])
+                nonzero_balances[balance['asset']] = value
+            except KeyError:
+                print(balance['locked'] + " " + balance['asset'])
+                nonzero_balances[balance['asset']] = float(balance['locked'])
+        else:
+            continue
+    return nonzero_balances
+
+
+if __name__ == '__main__':
+    print_header()
+    main()
