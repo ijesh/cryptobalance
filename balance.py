577871d45a09f5e55b883b74a84b82eb47764565
igiri <giriijesh@gmail.com>
2018-01-21 23:35:06 -0800

    Adding bittrex support

--- balance.py
+++ balance.py
@@ -5,15 +5,16 @@ from time import sleep
 from sys import platform
 from os import system
 from datetime import datetime
+from bittrex_balance import MyBittrex
 
 HEADER = '''\
-_________                        __                         __         .__
-\_   ___ \_______ ___.__._______/  |_  ______  _  _______ _/  |_  ____ |  |__
-/    \  \/\_  __ <   |  |\____ \   __\/  _ \ \/ \/ /\__  \\\\   __\/ ___\|  |  \\
-\     \____|  | \/\___  ||  |_> >  | (  <_> )     /  / __ \|  | \  \___|   Y  \\
-\______  /|__|   / ____||   __/|__|   \____/ \/\_/  (____  /__|  \___  >___|  /
-       \/        \/     |__|                             \/          \/     \/
-
+_________                        __        ___.          .__
+\_   ___ \_______ ___.__._______/  |_  ____\_ |__ _____  |  | _____    ____   ____  ____
+/    \  \/\_  __ <   |  |\____ \   __\/  _ \| __ \\__  \ |  | \__  \  /    \_/ ___\/ __ \
+\     \____|  | \/\___  ||  |_> >  | (  <_> ) \_\ \/ __ \|  |__/ __ \|   |  \  \__\  ___/
+ \______  /|__|   / ____||   __/|__|  \____/|___  (____  /____(____  /___|  /\___  >___  >
+        \/        \/     |__|                   \/     \/          \/     \/     \/    \/
+        
          Created by: Ijesh Giri
 '''
 
@@ -43,44 +44,55 @@ def main():
     args = process_args()
     with open(args.config) as keys:
         info_dict = yaml.load(keys)
-        api_key = info_dict['binance']['APIKey']
-        api_secret = info_dict['binance']['Secret']
+        binance_key = info_dict['binance']['APIKey']
+        binance_secret = info_dict['binance']['Secret']
+        client = Client(binance_key, binance_secret)
+        # watch_loop(client, 30)
+        total = get_total(client)
 
-        client = Client(api_key, api_secret)
-        watch_loop(client, 5)
+        bittrex_key = info_dict['bittrex']['APIKey']
+        bittrex_secret = info_dict['bittrex']['Secret']
+        myBittrex = MyBittrex(bittrex_key, bittrex_secret)
+        total += myBittrex.get_balance()
+
+        print("\n\nTotal: "+ str(round(total, 2)))
 
 def watch_loop(client, sleep_time = 30):
     try:
         while True:
-            btc_price_usd = get_currency_price_in_usd(client, "BTC")
-            eth_price_usd = get_currency_price_in_usd(client, "ETH")
-
-            print("Fetched at: "+ str(datetime.now()))
-            account_info = client.get_account()
-            nonzero_balances = get_nonzero_balances(account_info['balances'])
-            total = 0
-            for key, value in nonzero_balances.iteritems():
-                if(key == "BCX" or key == "SBTC"):
-                    continue
-                currency_price_usd = 0.00
-                try:
-                    currency_price_usd = get_currency_price_in_usd(client, key)
-                except:
-                    try:
-                        currency_price_usd = get_currency_price_in_usd(client, key, "BTC") * btc_price_usd
-                    except:
-                        currency_price_usd = get_currency_price_in_usd(client, key, "ETH") * eth_price_usd
-                symbolValue = value * currency_price_usd
-                if(symbolValue > 1000):
-                    print(key + ": " + str(symbolValue) + " USD")
-                total += symbolValue
+            total = get_total(client)
             print("Total balance: " + str(round(total, 2)) + " USD")
             sleep(sleep_time) #every 30 secs
             print("\n*****************************************************\n")
-
     except KeyboardInterrupt:
         clear()
         print_header()
+
+def get_total(client):
+    btc_price_usd = get_currency_price_in_usd(client, "BTC")
+    eth_price_usd = get_currency_price_in_usd(client, "ETH")
+
+    print("Fetched at: "+ str(datetime.now()))
+    account_info = client.get_account()
+    nonzero_balances = get_nonzero_balances(account_info['balances'])
+    total = 0
+    for key, value in nonzero_balances.iteritems():
+        if(key == "BCX" or key == "SBTC"):
+            continue
+        currency_price_usd = 0.00
+        try:
+            currency_price_usd = get_currency_price_in_usd(client, key)
+        except:
+            try:
+                currency_price_usd = get_currency_price_in_usd(client, key, "BTC") * btc_price_usd
+            except:
+                currency_price_usd = get_currency_price_in_usd(client, key, "ETH") * eth_price_usd
+        symbolValue = value * currency_price_usd
+        if(symbolValue > 1000):
+            print(key + ": " + str(symbolValue) + " USD")
+        total += symbolValue
+
+    return total
 
 def get_nonzero_balances(balances):
     nonzero_balances = {}
@@ -95,7 +107,6 @@ def get_nonzero_balances(balances):
 def get_currency_price_in_usd(client, currency, base_currency="USDT"):
     currency_pair = "{}{}".format(currency, base_currency)
     api_response = client.get_symbol_ticker(symbol=currency_pair)
-    # print(api_response)
     return float(api_response['price'])
 
 if __name__ == '__main__':
---
+++ bittrex_balance.py
@@ -0,0 +1,37 @@
+from bittrex.bittrex import Bittrex
+
+class MyBittrex():
+    def __init__(self, key, secret):
+        self.mybittrex = Bittrex(key, secret)
+        self.ethusd = float(self.get_usd("ETH")['result']['Last'])
+        self.btcusd = float(self.get_usd("BTC")['result']['Last'])
+
+    def get_balance(self):
+        balances = self.mybittrex.get_balances()
+        total = 0.0
+        for balance in balances['result']:
+            currency = balance['Currency']
+            balance = float(balance['Balance'])
+            if(balance > 0):
+                try:
+                    usd = 0.0
+                    payload = self.get_usd(currency)
+                    if(payload['success'] == False):
+                        payload = self.get_usd(currency, "BTC")
+                        if(payload['success'] == False):
+                            payload = self.get_usd(currency, "ETH")
+                            usd = balance * float(payload['result']['Last']) * self.ethusd
+                        else:
+                            usd = balance * float(payload['result']['Last']) * self.btcusd
+                    else:
+                        # print(payload)
+                        usd = float(payload['result']['Last']) * balance
+                    total += usd
+                    print(currency + ": "+ str(usd))
+                except:
+                    print("Error retrieving usd value for symbol: "+ currency)
+        return total
+
+    def get_usd(self, curr, base_currency = "USDT"):
+        currency_pair = "{}-{}".format(base_currency, curr)
+        return self.mybittrex.get_ticker(currency_pair)
